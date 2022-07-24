d770a503a0f398d09ffcb1f769a96dff4b726738
Ahsan Ali <60280488+Phant0m-a@users.noreply.github.com>
2022-07-24 04:43:08 -0700

    updated views and app.js
    added remove,add,edit cart routes
    added cart view route

--- app.js
+++ app.js
@@ -17,12 +17,26 @@ app.use(express.urlencoded({
 }));
 
 function isProductInCart(cart, id) {
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < cart.length; i++) {
         if (cart[i].id == id) {
             return true;
         }
     }
     return false;
+}
+
+function calculateTotal(cart, req) {
+    total = 0;
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].sale_price) {
+            total = total + (cart[i].sale_price * cart[i].quantity)
+        } else {
+            total = total + (cart[i].price * cart[i].quantity)
+        }
+        req.session.total = total;
+        return total;
+
+    }
 }
 
 const mysql = require('mysql');
@@ -48,10 +62,15 @@ db.connect((error) => {
 
 
 app.get("/", async (req, res) => {
-    db.query("SELECT * FROM products", (err, result) => {
-        res.render("index", { result: result });
-    })
 
+    try {
+        db.query("SELECT * FROM products", (err, result) => {
+            res.render("index", { result: result });
+        })
+
+    } catch (err) {
+        res.send('error accured!', { message: err });
+    }
 
 });
 
@@ -61,7 +80,8 @@ app.post("/add_to_cart", async (req, res) => {
 
         let { id, name, price, sale_price, quantity, image } = req.body;
         var product = { id: id, name: name, price: price, sale_price: sale_price, quantity: quantity, image: image };
-
+        //1.take id and quantity. (only as user can edit other stuff and th)
+        //2. use db request to get price using id then multiy by quantity to get total.  
 
         if (req.session.cart) {
             var cart = req.session.cart;
@@ -72,14 +92,87 @@ app.post("/add_to_cart", async (req, res) => {
             req.session.cart = [product];
             var cart = req.session.cart;
         }
+
+
+        calculateTotal(cart, req);
+
+        res.redirect('/cart');
     } catch (err) {
-
+        res.send('error accured!', { message: err });
     }
-
 
 });
 
+app.get('/cart', async (req, res) => {
+    try {
+        var cart = req.session.cart;
+        var total = req.session.total;
 
+        res.render('cart', { total, cart })
+
+    } catch (err) {
+        res.send('error accured!', { message: err });
+    }
+})
+
+app.post('/remove_product', async (req, res) => {
+
+    try {
+
+        var { id } = req.body;
+        var cart = req.session.cart;
+
+        for (let i = 0; i < cart.length; i++) {
+            if (cart[i].id == id) {
+                cart.splice(cart.indexOf(i), 1);
+            }
+
+            //re-calculate the price total
+            calculateTotal(cart, req);
+            res.redirect('/cart');
+
+        }
+
+        res.redirect('/cart');
+    } catch (err) {
+        res.send('error accured!', { message: err });
+    }
+})
+
+app.post('/edit_product_quantity', (req, res) => {
+    var { id,
+        quantity,
+        increase_product_quantity,
+        decrease_product_quantity } = req.body;
+    var cart = req.session.cart;
+    // var total = req.session.total;
+
+    if (increase_product_quantity) {
+        for (let i = 0; i < cart.length; i++) {
+            //get product with matching id and increase quantity
+            if (cart[i].id == id) {
+                if (cart[i].quantity > 0) {
+                    cart[i].quantity = parseInt(cart[i].quantity) + 1;
+                }
+            }
+        }
+    }
+
+    if (decrease_product_quantity) {
+        for (let i = 0; i < cart.length; i++) {
+            //get product with matching id and increase quantity
+            if (cart[i].id == id) {
+                if (cart[i].quantity > 1) {
+                    cart[i].quantity = parseInt(cart[i].quantity) - 1;
+                }
+            }
+        }
+    }
+    //re-calculate the price total
+    calculateTotal(cart, req);
+    res.redirect('/cart');
+
+})
 
 
 app.listen(5000, () => console.log("Pomato Server Started on Port 5000"));
