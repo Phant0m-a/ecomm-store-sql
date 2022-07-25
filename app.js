3975300cd04ee8dc4db7e3fc4f8b3a86fc71cf5b
Ahsan Ali <60280488+Phant0m-a@users.noreply.github.com>
2022-07-25 00:04:17 -0700

    updated app.js
    fixed cart bug added contact us suppport , fixed queries and minor bug fixes

--- app.js
+++ app.js
@@ -26,17 +26,20 @@ function isProductInCart(cart, id) {
 }
 
 function calculateTotal(cart, req) {
-    total = 0;
+    let total = 0;
+    console.log('cart at calculation time\n', cart);
     for (let i = 0; i < cart.length; i++) {
         if (cart[i].sale_price) {
+
             total = total + (cart[i].sale_price * cart[i].quantity)
         } else {
             total = total + (cart[i].price * cart[i].quantity)
         }
-        req.session.total = total;
-        return total;
+
 
     }
+    req.session.total = total;
+    return total;
 }
 
 const mysql = require('mysql');
@@ -61,6 +64,33 @@ db.connect((error) => {
 
 
 
+app.get("/invoice", async (req, res) => {
+    try {
+        // do some action
+        res.redirect('/');
+    } catch (err) {
+        res.status(401).send({ success: false, message: 'something went wrong', err: err })
+    }
+});
+app.post("/contact-us", async (req, res) => {
+    try {
+        const { name, email, phone, message } = req.body;
+        // add db-schema and query
+        let query = "INSERT INTO contact_us(name,email,phone,message) VALUES ?";
+        let values = [[name, email, phone, message]];
+        //uplaod data to db
+        db.query(query, [values], (err, result) => {
+
+            res.redirect('/');
+        })
+
+
+    } catch (err) {
+        res.status(401).send({ success: false, message: 'something went wrong', err: err })
+    }
+});
+
+
 app.get("/", async (req, res) => {
 
     try {
@@ -69,7 +99,7 @@ app.get("/", async (req, res) => {
         })
 
     } catch (err) {
-        res.send('error accured!', { message: err });
+        res.status(401).send({ success: false, message: 'something went wrong', err: err })
     }
 
 });
@@ -79,39 +109,42 @@ app.post("/add_to_cart", async (req, res) => {
     try {
 
         let { id, name, price, sale_price, quantity, image } = req.body;
-        var product = { id: id, name: name, price: price, sale_price: sale_price, quantity: quantity, image: image };
+        var product = { id: parseInt(id), name: name, price: parseFloat(price), sale_price: parseFloat(sale_price), quantity: parseInt(quantity), image: image };
         //1.take id and quantity. (only as user can edit other stuff and th)
         //2. use db request to get price using id then multiy by quantity to get total.  
-
+        let cart;
         if (req.session.cart) {
-            var cart = req.session.cart;
+            cart = await req.session.cart;
             if (!isProductInCart(cart, id)) {
                 cart.push(product);
+                console.log('added new product', cart);
+                req.session.cart = cart;
+                calculateTotal(cart, req);
             }
         } else {
             req.session.cart = [product];
-            var cart = req.session.cart;
+            cart = await req.session.cart;
+            calculateTotal(cart, req);
         }
 
 
-        calculateTotal(cart, req);
 
         res.redirect('/cart');
     } catch (err) {
-        res.send('error accured!', { message: err });
+        res.status(401).send({ success: false, message: 'something went wrong', err: err })
     }
 
 });
 
 app.get('/cart', async (req, res) => {
     try {
-        var cart = req.session.cart;
-        var total = req.session.total;
+        let cart = req.session.cart;
+        let total = req.session.total;
 
         res.render('cart', { total, cart })
 
     } catch (err) {
-        res.send('error accured!', { message: err });
+        res.status(401).send({ success: false, message: 'something went wrong', err: err })
     }
 })
 
@@ -135,7 +168,7 @@ app.post('/remove_product', async (req, res) => {
 
         res.redirect('/cart');
     } catch (err) {
-        res.send('error accured!', { message: err });
+        res.status(401).send({ success: false, message: 'something went wrong', err: err })
     }
 })
 
@@ -153,6 +186,8 @@ app.post('/edit_product_quantity', (req, res) => {
             if (cart[i].id == id) {
                 if (cart[i].quantity > 0) {
                     cart[i].quantity = parseInt(cart[i].quantity) + 1;
+                    //re-calculate whole in-crement
+                    calculateTotal(cart, req);
                 }
             }
         }
@@ -164,12 +199,13 @@ app.post('/edit_product_quantity', (req, res) => {
             if (cart[i].id == id) {
                 if (cart[i].quantity > 1) {
                     cart[i].quantity = parseInt(cart[i].quantity) - 1;
+                    //re-calculate whole de-crement
+                    calculateTotal(cart, req);
                 }
             }
         }
     }
-    //re-calculate the price total
-    calculateTotal(cart, req);
+
     res.redirect('/cart');
 
 })
@@ -179,7 +215,7 @@ app.get('/checkout', async (req, res) => {
         var total = req.session.total;
         res.render('checkout', { total });
     } catch (err) {
-        res.send('error accured', err);
+        res.status(401).send({ success: false, message: 'something went wrong', err: err })
     }
 })
 
@@ -187,17 +223,17 @@ app.get('/checkout', async (req, res) => {
 app.post('/place-order', async (req, res) => {
     try {
 
-        // const { name,
-        //     email,
-        //     phone,
-        //     city,
-        //     address,
-        // } = req.body;
-        name = 'demo';
-        email = 'demo@gmail.com';
-        phone = 03455555555;
-        city = 'lahore';
-        address = 'fake address';
+        let { name,
+            email,
+            phone,
+            city,
+            address,
+        } = req.body;
+        // name = 'demo';
+        // email = 'demo@gmail.com';
+        // phone = 03455555555;
+        // city = 'lahore';
+        // address = 'fake address';
         let cost = req.session.total;
         var products_ids = '';
         let status = 'not paid';
@@ -209,16 +245,16 @@ app.post('/place-order', async (req, res) => {
         for (let i = 0; i < cart.length; i++) {
             products_ids = products_ids + "," + cart[i].id;
         }
-        var query = "INSERT INTO orders(cost,name,email,status,city,address,phone,date,products_ids) VALUES ?";
-        var values = [[cost, name, email, status, city, address, phone, date, products_ids]];
+        let query = "INSERT INTO orders(cost,name,email,status,city,address,phone,date,products_ids) VALUES ?";
+        let values = [[cost, name, email, status, city, address, phone, date, products_ids]];
         //uplaod data to db
         db.query(query, [values], (err, result) => {
-            console.log('here', query);
+
             res.redirect('/payment');
         })
         // res.render('checkout');
     } catch (err) {
-        res.send('error accured', err);
+        res.status(401).send({ success: false, message: 'something went wrong', err: err })
     }
 })
 
@@ -228,9 +264,10 @@ app.get('/payment', async (req, res) => {
         let total = req.session.total;
         res.render('payment', { total })
     } catch (err) {
-        res.send('error accured', err);
+        res.status(401).send({ success: false, message: 'something went wrong', err: err })
     }
 })
+
 
 
 app.listen(5000, () => console.log("Pomato Server Started on Port 5000"));
